c44986aa1b9cf5f3fe8d1c4635a41da3a4c551f6
Jacob M <jmielc2@uic.edu>
2020-12-19 20:34:47 -0600

    Morse Code Project - 12/19/2020
    Rounded out the Morse Game Program and did some small changes to the Morse Code Converter Program (now it just returns to the main menu when it is done, it doesn't ask if you want to convert something else. Also made a small fix to an mistake in the eng-to-morse-dict dictionary file.

--- MorseCodeConverter.py
+++ MorseCodeConverter.py
@@ -7,7 +7,10 @@ Program Function: When called, this function will show three options: (1)
 Convert from English to Morse Code, (2) Convert Morse Code into English,
 and (3) return to the main menu (the Main Program).
 '''
-#Conversion Functions
+#Import statements
+import time
+from eng_to_morse_dict import english_morse_dict as eng_morse
+
 #Converts English to Morse Code
 def english_to_morse(phrase, eng_morse):
     '''This function converts english into morse code'''
@@ -19,6 +22,7 @@ def english_to_morse(phrase, eng_morse):
         result_list.append(bit)
     result = ' '.join(result_list)
     return result
+
 
 #Converts Morse Code to English
 def morse_to_english(code, eng_morse):
@@ -47,10 +51,9 @@ def morse_to_english(code, eng_morse):
     result = ' '.join(result_list)
     return result
 
-def convert_program():
-    #Imports
-    from eng_to_morse_dict import english_morse_dict as eng_morse
 
+#This is the main convert_program
+def convert_program():
     #Variables
     rep = 0
     options = ['1', '2', '3']
@@ -60,49 +63,48 @@ def convert_program():
     print("\nOPTIONS MENU")
     print("  - (1) Translate English to Morse Code")
     print("  - (2) Translate Morse Code to English")
-    print("  - (3) Return to MAIN MENU")
 
     #Conversion Option
     option = input("Please Select an Option: ")
 
     #Conversion Loop
-    while option != '3':
-        if option not in options:
-            option = input("Invalid Entry, Please Enter a Valid Option: ")
+    if option not in options:
+        option = input("Invalid Entry, Please Enter a Valid Option: ")
 
-        if option == '1':
-            print("\nEnter English Text (no symbols: . ; , - etc.):")
-            phrase = input().lower().strip()
-            result = english_to_morse(phrase, eng_morse)
-            if isinstance(result, str) != True:
-                print("\n'{}' is a character in your string that cannont be converted to Morse Code.".format(result[0]))
-            else:
-                if rep < 1:
-                    print("\nRESULT FORMATTING:")
-                    print("  - '.' = DOT")
-                    print("  - '-' = DASH")
-                    print("  - ' ' = Separates Letters")
-                    print("  - '/' = Separate Words")
-                    rep = 1
-                print("\nTRANSLATION:", result)
-            result = ''
-            option = input("Please select what you'd like to do next from the OPTIONS MENU: ")
+    if option == '1':
+        print("\nEnter English Text (no symbols: . ; , - etc.):")
+        phrase = input().lower().strip()
+        result = english_to_morse(phrase, eng_morse)
+        if isinstance(result, str) != True:
+            print("\n'{}' is a character in your string that cannont be converted to Morse Code.".format(result[0]))
+        else:
+            if rep < 1:
+                print("\nRESULT FORMATTING:")
+                print("  - '.' = DOT")
+                print("  - '-' = DASH")
+                print("  - ' ' = Separates Letters")
+                print("  - '/' = Separate Words")
+                rep = 1
+            print("\nTRANSLATION:", result)
+        result = ''
+        option = input("Please select what you'd like to do next from the OPTIONS MENU: ")
 
-        if option == '2':
-            print("\nINPUT FORMAT:")
-            print("  - '.' = DOT")
-            print("  - '-' = DASH")
-            print("  - ' ' = Separates Letters")
-            print("  - '/' and '\\' = Separate Words")
-            print("Enter Morse Code:")
-            code = input()
-            result = morse_to_english(code, eng_morse)
-            if isinstance(result, str) != True:
-                print("\n'{}' is not a Morse Code character.".format(result[0]))
-            else:
-                print("\nTRANSLATION:", result.capitalize())
-            result = ''
-            option = input("Please Select What You'd like to do Next from the OPTIONS MENU: ")
+    if option == '2':
+        print("\nINPUT FORMAT:")
+        print("  - '.' = DOT")
+        print("  - '-' = DASH")
+        print("  - ' ' = Separates Letters")
+        print("  - '/' and '\\' = Separate Words")
+        print("Enter Morse Code:")
+        code = input()
+        result = morse_to_english(code, eng_morse)
+        if isinstance(result, str) != True:
+            print("\n'{}' is not a Morse Code character.".format(result[0]))
+            time.sleep(3)
+        else:
+            print("\nTRANSLATION:", result.capitalize())
+            time.sleep(3)
+        result = ''
 
-    print("\nReturning to MAIN MENU...")
+    print("\n\nReturning to MAIN MENU...")
     print("\n----------------------------------------------------------------------")
--- MorseCodeGame.py
+++ MorseCodeGame.py
@@ -10,58 +10,106 @@ user will then input their translation. The program will then convert the word a
 translation is true of false. If the answer is wrong, it will print the correct result. After ten repetitions
 the program will print a score and return to the main menu.
 '''
+#Import statements
+from eng_to_morse_dict import english_morse_dict as dictionary
+from MorseCodeConverter import english_to_morse
+import random, time
+
+
+#Obtain Game Options
+def game_options():
+    '''This function runs through a series of dialog and inputs where the user specifies
+    the settings of the game they want to play'''
+    options = ['1', '2']
+    option1 = True
+    option2 = True
+    #Chooses a language to convert to
+    print("\n----------------------------------------------------------------------")
+    print("\nCHOOSE A LANGUAGE:\n")
+    print("  - Enter (1) to test your Morse Code to English conversion skills")
+    print("  - Enter (2) to test your English to Morse Code conversion skills")
+    language = input("Please Select An Option: ")
+    while option1:
+        if language not in options:
+            print("Invalid Entry, Please Enter a Valid Option:")
+        else:
+            option1 = False
+    #Chooses whether it will be testing letters or words
+    print("\nCHOOSE A MODE:\n")
+    print("  - Enter (1) to convert Letters")
+    print("  - Enter (2) to convert Words")
+    mode = input("Please Select An Option: ")
+    while option2:
+        if mode not in options:
+            print("Invalid Entry, Please Enter a Valid Option:")
+        else:
+            option2 = False
+    return (language, mode)
+
+
+def choose_Phrase(language, mode):
+    '''This function returns a phrase from either the dictionary or the Phrases.txt
+    document depending on the settings which are specified as the parameters'''
+    if language == '1': #If user is converting from Morse Code to English
+        if mode == '1': #If user is converting just singular letters
+            letters = dictionary.keys()
+            rand_num = random.randint(0, len(letters) - 1)
+            chosen_letter = list(letters)[rand_num]
+            morse_letter = dictionary[chosen_letter]
+            return (morse_letter, chosen_letter)
+        else: #If user is converting phrases and words
+            all_phrases = open("Phrases.txt", 'r')
+            phrases_list = all_phrases.readlines()
+            all_phrases.close()
+            rand_num = random.randint(0, len(phrases_list) - 1)
+            chosen_phrase = phrases_list[rand_num]
+            new_phrase = chosen_phrase[:-1]  #Removes the \n character
+            morse_phrase = english_to_morse(new_phrase.lower(), dictionary)
+            return (morse_phrase, new_phrase)
+    else: #If user is converting from English to Morse Code
+        if mode == '1': #If user is converting just singular letters
+            pass #TODO: Finish the code where you convert English to Morse Code for letters
+        else: #If user is converting phrases and words
+            pass #TODO: Finish the code where you convert English to Morse Code for phrases
+
 
 def game_program():
-    import random
-    from eng_to_morse_dict import english_morse_dict as dictionary
-    from MorseCodeConverter import convert_program
-    options = ['1', '2']
-
-    #Obtain Game Options
-    def game_options():
-        option1 = True
-        option2 = True
-        #Chooses a language to convert to
-        print("\n----------------------------------------------------------------------")
-        print("\nCHOOSE A LANGUAGE:\n")
-        print("  - Enter (1) for Morse Code to English")
-        print("  - Enter (2) for English to Morse Code")
-        language = input("\nPlease Select An Option: ")
-        while option1:
-            if language not in options:
-                print("Invalid Entry, Please Enter a Valid Option:")
-            else:
-                option1 = False
-        #Chooses whether it will be testing letters or words
-        print("\nCHOOSE A MODE:\n")
-        print("  - Enter (1) for Letters")
-        print("  - Enter (2) for Words")
-        mode = input("\nSelect One: ")
-        while option2:
-            if mode not in options:
-                print("Invalid Entry, Please Enter a Valid Option:")
-            else:
-                option2 = False
-        return (language, mode)
-
-    #Choose A Phrase
-    def choose_Phrase(language, mode):
-        if language == '1':
-            if mode == '1':
-                #FIXME: Create a list of Morse Code letters to be used for 
-                #the game
-                pass
-            else:
-                all_phrases = open("Phrases.txt", 'r')
-                phrases_list = all_phrases.readlines()
-                all_phrases.close()
-        rand_num = random.randint(0, len(phrases_list))
-        chosen_phrase = phrases_list[rand_num]
-        return chosen_phrase
-    
-    print("\n----------------------------------------------------------------------")
-    print("\nGAME PROGRAM RUNS THEN QUITS HERE")
-    print("\nReturning to MAIN MENU...")
-    print("\n----------------------------------------------------------------------")
-
-#game_program
+    '''This function is the skeleton of the game and runs all the previous functions
+    in certain sequence as the game'''    
+    print('\nBeginning Game...')
+    selections = game_options()
+    tested_list = [' ']
+    score = 0
+    rep = 1
+    time.sleep(3)
+    while rep <= 10:
+        phrases = choose_Phrase(selections[0], selections[1])
+        while phrases[1] in tested_list:
+            phrases = choose_Phrase(selections[0], selections[1])
+        print('\n----------------------------------------------------------------------')
+        print("\n #{} - TRANSLATE:".format(rep), phrases[0])
+        user_answer = input("YOUR TRANSLATION: ")
+        if user_answer.lower().strip() != phrases[1].lower():
+            print("\nIncorrect, the correct translation was '{}'".format(phrases[1]))
+            time.sleep(2)
+        else:
+            print('\nCorrect!')
+            score += 1
+            tested_list.append(phrases[1])
+            time.sleep(2)
+        rep += 1
+    print('\n----------------------------------------------------------------------')
+    print("\nLet's see how you did...")
+    time.sleep(2)
+    final_score = score / 10
+    print('\nFinal Score: {}%'.format(int(final_score * 100)))
+    time.sleep(1)
+    if final_score <= 0.5:
+        print("\nKeep at it! you'll get it someday!")
+    elif final_score <= 0.9:
+        print("\nNice Job! You almost got a prefect score!")
+    else:
+        print("\nPerfect Score! Great Job!")
+    time.sleep(1)
+    print('\n\nReturning to MAIN MENU...')
+    print ('\n----------------------------------------------------------------------')
