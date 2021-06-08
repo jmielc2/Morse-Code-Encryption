ce6d3f543dd9bba5f1c23ed1833204d6ab41b0ba
Jacob M <jmielc2@uic.edu>
2021-06-07 23:09:29 -0500

    Added Something small I learned
    Added the "if __name__ == "__main__" " piece of the main program file.

--- MainProgram.py
+++ MainProgram.py
@@ -20,36 +20,40 @@ morse code.
 If they want to convert, they will input a statement in either morse code or
 written language which will then be converted into the opposite language.
 '''
+
 #Import Statements
 from MorseCodeGame import game_program
 from MorseCodeConverter import convert_program
+def main():
+    options = ['1', '2', '3']
 
-options = ['1', '2', '3']
+    #Functions
+    request = 0
+    def request_func():
+        print("\nMAIN MENU:")
+        print("  - Enter (1) to Play the Learn Morse Code Game")
+        print("  - Enter (2) to Translate")
+        print("  - Enter (3) to Quit")
+        request = input("What Would You Like To Do?: ")
+        while request not in options:
+            request = input("Invalid Entry, Please Enter a Valid Option: ")
+        return request
 
-#Functions
-request = 0
-def request_func():
-    print("\nMAIN MENU:")
-    print("  - Enter (1) to Play the Learn Morse Code Game")
-    print("  - Enter (2) to Translate")
-    print("  - Enter (3) to Quit")
-    request = input("What Would You Like To Do?: ")
-    while request not in options:
-        request = input("Invalid Entry, Please Enter a Valid Option: ")
-    return request
+    #What do you want to do?
+    request = request_func()
 
-#What do you want to do?
-request = request_func()
+    while request != '3':
+        #Game/Learn
+        if request == '1':
+            game_program()
+            request = request_func()
 
-while request != '3':
-    #Game/Learn
-    if request == '1':
-        game_program()
-        request = request_func()
+        #Convert
+        if request == '2':
+            convert_program()
+            request = request_func()
 
-    #Convert
-    if request == '2':
-        convert_program()
-        request = request_func()
+    print("\nGoodbye")
 
-print("\nGoodbye")
+if __name__ == "__main__":
+    main()
--- setup.py
+++
@@ -1,3 +0,0 @@
-from cx_Freeze import Executable, setup
-
-setup (name = "Morse_Code_Converter" , version = "0.1", description = "", executables = [Executable("MainProgram.py")])
